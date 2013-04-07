b9b34835226375ddf24e82afa062537aec3df6d4
gljivar <gljivar@gmail.com>
2013-04-07 22:39:15 +0200

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tournament::Application.config.secret_token = 'e5f845080d4f45fff8842d3b2e03a4d443d966d382ccd47c4acba59b61764006cf561cb5e73b51d5466ab4d048a2d703ba77e035a7b92b7bc09043c6aa46c48a'
