e0fc508063230192399743b2bce9ad7698c520d5
gljivar <gljivar@gmail.com>
2013-04-12 20:20:15 +0200

    Add initial and first name, and full name

--- app/models/competitor.rb
+++ app/models/competitor.rb
@@ -2,4 +2,12 @@ class Competitor < ActiveRecord::Base
   belongs_to :category
   belongs_to :club
   attr_accessible :lot_number, :first_name, :last_name, :category_id, :club_id
+
+  def full_name
+    first_name + ' ' + last_name
+  end
+
+  def initial_and_last_name
+    first_name[0, 1] + '. ' + last_name
+  end
 end
