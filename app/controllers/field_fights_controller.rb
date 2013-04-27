ccdd4023e39e08d74aeb72d2fec6531f39164f19
gljivar <gljivar@gmail.com>
2013-04-27 21:12:25 +0200

    Working on Field fights

--- app/controllers/field_fights_controller.rb
+++ app/controllers/field_fights_controller.rb
@@ -14,9 +14,9 @@ class FieldFightsController < ApplicationController
       @fights_current = Fight.find_by_sql ["SELECT * FROM (SELECT * FROM (SELECT * FROM fights WHERE field_actual_id = ? AND competitor_winner_id IS NOT NULL ORDER BY number DESC LIMIT ?) UNION ALL SELECT * FROM (SELECT * FROM fights WHERE field_actual_id = ? AND competitor_winner_id IS NULL ORDER BY number LIMIT ?))", @field_actual_id, @limit_previous, @field_actual_id, @limit_next] 
 
       @fights = @fights + @fights_current
-      @fights.each do |fight|  
-        field.fights_actual.push(fight) 
-      end
+#      @fights.each do |fight|  
+#        field.fights_actual.push(fight) 
+#      end
     end
 
     respond_to do |format|
@@ -24,6 +24,8 @@ class FieldFightsController < ApplicationController
       #format.json { render json: @fields.to_json( :include => :fights_actual)  }
       #format.json { render json: @fields.to_json( :include => { :fights_actual => { :include => { :field_initial => { :only => [:name] }, :field_actual => { :only => [:name] }, :category => { :only => [:name] }, :competitor_blue => { :only => [:last_name] }, :competitor_red => { :only => [:last_name] }, :competitor_winner => { :only => [:last_name] }, :previous_fight_blue => { :only => [:number] }, :previous_fight_red => { :only => [:number] } } } } ) }
       
+      #format.json { render json: JSON::parse(@fights.to_json( :include => { :field_initial => { :only => [:name] }, :field_actual => { :only => [:name] }, :category => { :only => [:name] }, :competitor_blue => { :only => [:last_name] }, :competitor_red => { :only => [:last_name] }, :competitor_winner => { :only => [:last_name] }, :previous_fight_blue => { :only => [:number] }, :previous_fight_red => { :only => [:number] } } ) ) }
+
       format.json { render json: @fights.to_json( :include => { :field_initial => { :only => [:name] }, :field_actual => { :only => [:name] }, :category => { :only => [:name] }, :competitor_blue => { :only => [:last_name] }, :competitor_red => { :only => [:last_name] }, :competitor_winner => { :only => [:last_name] }, :previous_fight_blue => { :only => [:number] }, :previous_fight_red => { :only => [:number] } } ) }
     end
 
