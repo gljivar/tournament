8c80147f93be2e612671acae9ddcea31dd6f966a
gljivar <gljivar@gmail.com>
2013-05-05 23:48:39 +0200

    Changed login logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,20 +14,25 @@ skip_before_filter :require_login
     username = params[:username]
     password = params[:password]
 
-    if username == 'miro' and password == 'miro'
-      user = { :user_id => 1, :username => username, :logged_in => true }
-      session[:user_id] = 1 
+    if User.count == 0
+      user = User.new
+      user.username = 'admin'
+      user.password = 'admin'
+      user.save
+
+      username = user.username
+      password = user.password 
+    end 
+
+    user = User.find_by_username_and_password(username, password)
+
+    if user.username == username  
+      user_j = { :user_id => user.id, :username => user.username, :logged_in => true }
+      session[:user_id] = user.id 
 
       respond_to do |format|
         format.html
-        format.json { render :json => user.to_json } # user.to_json }
-      end
-    elsif session[:user_id] != nil
-      user = { :user_id => 1, :username => username, :logged_in => true }
-
-      respond_to do |format|
-        format.html
-        format.json { render :json => user.to_json } # user.to_json }
+        format.json { render :json => user_j.to_json }
       end
     else
       respond_to do |format|
