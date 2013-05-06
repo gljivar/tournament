9f9d28d1306703918d6681842808ffeca2395fc0
Mieszko4 <mieszko4@gmail.com>
2013-05-06 03:06:00 +0200

    Cleaned up NavigationCtrl and fixed nav not hiding after click

--- app/assets/javascripts/navigation_controller.js
+++ app/assets/javascripts/navigation_controller.js
@@ -1,21 +1,22 @@
 var root = typeof global !== "undefined" && global !== null ? global : window;
 
 root.NavigationCtrl = function ($scope, $http, $location, LogService, TournamentGlobal) {
-                $scope.TournamentGlobal = TournamentGlobal;
-                $scope.success = true;
-                $scope.goToView = function (view) {
-                        $location.path(view);
-                };
-                $scope.logout = function () {
-                        $scope.success = LogService.logout();
-                        $location.path('/');
-                };
-                //$scope.myPlaylists = function () {
-                //        $location.path('/user/' + ListUp.userId + '/playlists');
-                //};
-
-                //$scope.myProfile = function () {
-                //        $location.path('/users/' + ListUp.userId);
-                //};
-        };
-
+	$scope.TournamentGlobal = TournamentGlobal;
+	$scope.success = true;
+	
+	$scope.goToView = function (view) {
+		$location.path(view);
+	};
+	$scope.logout = function () {
+		$scope.success = LogService.logout();
+		$location.path('/');
+	};
+	
+	$scope.collapseNav = function () {
+		$('.navbar .nav-collapse')
+			.removeClass('in')
+			.removeClass('collapse')
+			.removeAttr('style')
+		;
+	};
+};
