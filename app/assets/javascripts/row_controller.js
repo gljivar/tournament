ef1e76ce3fcc1a1324dae058d47a316b0232de2c
Mieszko4 <mieszko4@gmail.com>
2013-05-06 17:06:04 +0200

    Implemented hiding sidebar for /repeater

---
+++ app/assets/javascripts/row_controller.js
@@ -0,0 +1,16 @@
+var root = typeof global !== "undefined" && global !== null ? global : window;
+
+root.RowCtrl = function ($rootScope, $scope, $location) {
+	$rootScope.$on('$viewContentLoaded', function() {
+		var path = $location.path();
+		if (path === '/repeater') {
+			$scope.viewClass = 'span12';
+		        $scope.sidebarClass = '';
+			$scope.sidebarHide = true;
+		} else {
+			$scope.viewClass = 'span9';
+		        $scope.sidebarClass = 'span3';
+			$scope.sidebarHide = false;
+		}
+	});
+};
