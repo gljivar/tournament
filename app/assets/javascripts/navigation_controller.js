b1311649be388698b4045645449858228ad8c36b
Mieszko4 <mieszko4@gmail.com>
2013-05-07 16:55:16 +0200

    Fixed menu in responsive view after adding Other

--- app/assets/javascripts/navigation_controller.js
+++ app/assets/javascripts/navigation_controller.js
@@ -1,6 +1,30 @@
 var root = typeof global !== "undefined" && global !== null ? global : window;
 
 root.NavigationCtrl = function ($scope, $http, $location, LogService, TournamentGlobal) {
+	//collapse nav bar after click
+	$('.nav-collapse a[class!=dropdown-toggle]').on('click', function () {
+		$('.navbar .nav-collapse')
+			.removeClass('in')
+			.removeClass('collapse')
+			.removeAttr('style')
+		;
+	});
+	//fix height
+	$('.nav-collapse a[class=dropdown-toggle]').on('click', function (e) {
+		e.preventDefault();
+		var $this = $(this),
+			$parent = $this.parents('.dropdown:first'),
+			menuHeight = $parent.find('.dropdown-menu').innerHeight(),
+			mainMenuHeight = $('.navbar .nav-collapse').height(),
+			isOpen = $parent.hasClass('open');
+		
+		if (!isOpen) {
+			$('.navbar .nav-collapse').height(mainMenuHeight+menuHeight);
+		} else {
+			$('.navbar .nav-collapse').height(mainMenuHeight-menuHeight-2); //TODO: remove HACK!
+		}
+	});
+	
 	$scope.TournamentGlobal = TournamentGlobal;
 	$scope.success = true;
 	
@@ -10,13 +34,5 @@ root.NavigationCtrl = function ($scope, $http, $location, LogService, Tournament
 	$scope.logout = function () {
 		$scope.success = LogService.logout();
 		$location.path('/');
-	};
-	
-	$scope.collapseNav = function () {
-		$('.navbar .nav-collapse')
-			.removeClass('in')
-			.removeClass('collapse')
-			.removeAttr('style')
-		;
 	};
 };
